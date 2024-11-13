859f5cdc814059d35d4b2028e562e3c336df8617
Venita Kissell <venitak@hotmail.com>
2024-11-13 22:17:24 +1000

    feat: File write using both sync and async modes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-console.lot("This file is running!");
+console.log("This file is running!");
 
 let defaultEnv = {
     PORT: 5050,
     DATABASE_URI: "asdasd:asdasd",
     JWT_SECURITY_KEY: "asdasdas",
     SECERET_KEY: "apple",
-    SECURE_API_KEY: "sdfas765haskdhfahdfkahdsf"
+    SECURE_API_KEY: "sdfas765haskdhfahdfkahdsf" 
 };
+
+let  contentToWrite = "";
+//What should it look like
+// PORT=5050
+//DATABASE_URI: "asdasd:asdasd",
+//JWT_SECURITY_KEY: "asdasdas",
+//SECERET_KEY: "apple",
+//SECURE_API_KEY: "sdfas765haskdhfahdfkahdsf"
+
+Object.keys(defaultEnv).forEach(
+    envKey => {
+        // Add a line to the contentToWrite variable with the key name and value
+        contentToWrite += `${envKey}=${defaultEnv[envKey]}\n`;
+    }
+);
+
+console.log(contentToWrite);
+
+// All the filelhandling operations will be performed using fs
+const fs = require("node:fs");
+
+// Synchronous way
+//fs.writeFileSync(".env", contentToWrite);
+
+
+// Asynchronpis way
+// fs.writeFile(filepath, fileContents, callback); Error message function
+fs.writeFile(".env", contentToWrite, (error) => {
+    if (error) {
+        console.log("Error encountered: ", error);
+    }
+    else {
+        console.log("File written successfully!");
+    }
+})
+
+
+console.log("File has been written.");
