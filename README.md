bad00b5bbc97ba1785cf4fdd50987e702c54ac53
Venita Kissell <venitak@hotmail.com>
2024-11-13 21:46:53 +1000

    feat: project init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # t3w4-async-intro
+
+# Topics for today:
+- What is Asynchronous coding?
+- Threads, call stack, event loop, browser API's
+- JS Promises
+- .then callback functions
+- catch and finally 
+- File I/O with promises
---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.lot("This file is running!");
+
+let defaultEnv = {
+    PORT: 5050,
+    DATABASE_URI: "asdasd:asdasd",
+    JWT_SECURITY_KEY: "asdasdas",
+    SECERET_KEY: "apple",
+    SECURE_API_KEY: "sdfas765haskdhfahdfkahdsf"
+};
